bf58138d17ea367d012b5b679e26541df6ce7e67
wangjeifei1 <szwjf08@gmail.com>
2018-09-21 15:21:33 -0400

    wrong

--- R/Simulation/fake_j/fake.r
+++ R/Simulation/fake_j/fake.r
@@ -1,5 +1,10 @@
+getParms<-function(dispersion_x=0.8,dispersion_z=0.3,signal=1){
+  parms=list(dispersion_x=dispersion_x,dispersion_z=dispersion_z,signal=signal,
+             transf=function(x)log((x+1)),maxCount=100)
+  return(parms)
+}
 
-sim_circle<-function(geometry,parms){
+createRefPattern_circle<-function(geometry,parms){
 #large dispersion means large range
 dispersion_x=parms$dispersion_x
 dispersion_z=parms$dispersion_z
@@ -9,21 +14,46 @@ z=geometry$z
 anchor_point=c(sample(x,1),sample(z,1))
 scale_distance=1/(sqrt(c(var(x),var(z)))*c(dispersion_x,dispersion_z))
 
-geometry$intensity=0
+intensity=rep(0,length(x))
 for(i in 1:length(x)){
   distance=(anchor_point-c(x[i],z[i]))*scale_distance
   distance=sum(distance^2)
-  weight=pnorm(distance)
-  geometry$intensity[i]=2*min(weight,1-weight)*signal
+  weight=pnorm(distance,sd=signal)
+  intensity[i]=min(weight,1-weight)
 }
-return(geometry)
+intensity=(intensity-min(intensity))/(max(intensity)-min(intensity))
+
+return(intensity)
 }
 
-getParms<-function(dispersion_x=0.8,dispersion_z=0.3,signal=1){
-  parms=list(dispersion_x=dispersion_x,dispersion_z=dispersion_z,signal=signal)
-  return(parms)
+
+createdropSeqPattern<-function(refPattern,parms){
+  count=round(parms$transf(refPattern)*100)
+  return(count)
+}
+createPattern<-function(geometry,geneNum){
+  refTable=matrix(0,length(geometry$x),geneNum)
+  dropTable=matrix(0,length(geometry$x),geneNum)
+  for(i in 1:geneNum){
+  parms=getParms(signal = 1)
+  refPattern=createRefPattern_circle(geometry,parms)
+  dropPattern=createdropSeqPattern(refPattern,parms)
+  refTable[,i]=refPattern
+  dropTable[,i]=dropPattern
+  }
+pattern=list(refTable=refTable,dropTable=dropTable)
+return(pattern)
 }
 
-parms=getParms()
-result=sim_circle(geometry,parms)
-intensityPlot(result$intensity,result)
+patternData=createPattern(geometry,10)
+
+k=2
+intensityPlot(patternData$refTable[,k],geometry)
+
+
+library(GenomicRanges)
+
+
+
+
+
